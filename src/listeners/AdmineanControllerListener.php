0a87c098868564353701cefbdabf34d89382e829
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-12 19:09:46 +0100

    Moved filterForm to Listeners

--- src/listeners/AdmineanControllerListener.php
+++ src/listeners/AdmineanControllerListener.php
@@ -5,36 +5,22 @@ namespace VitesseCms\Shop\Listeners;
 use Phalcon\Events\Event;
 use VitesseCms\Admin\Forms\AdminlistFormInterface;
 use VitesseCms\Admin\Utils\AdminUtil;
+use VitesseCms\Shop\Controllers\AdmincountryController;
+use VitesseCms\Shop\Controllers\AdmineanController;
 use VitesseCms\Shop\Controllers\AdminorderController;
 use VitesseCms\Shop\Controllers\AdminorderstateController;
 use VitesseCms\Shop\Enum\OrderStateEnum;
 use VitesseCms\Shop\Models\OrderState;
 
-class AdminorderstateControllerListener
+class AdmineanControllerListener
 {
-    public function adminListItem(
-        Event $event,
-        AdminorderstateController $controller,
-        OrderState $orderState
-    ): void
-    {
-        switch ($orderState->getStockAction()) :
-            case OrderStateEnum::STOCK_ACTION_INCREASE:
-                $orderState->setAdminListName($orderState->getNameField() . ' +');
-                break;
-            case OrderStateEnum::STOCK_ACTION_DECREASE:
-                $orderState->setAdminListName($orderState->getNameField() . ' -');
-                break;
-        endswitch;
-    }
-
     public function adminListFilter(
         Event $event,
-        AdminorderstateController $controller,
+        AdmineanController $controller,
         AdminlistFormInterface $form
     ): string
     {
-        $form->addNameField($form);
+        $form->addText('%CORE_NAME%', 'filter[name]');
         $form->addPublishedField($form);
 
         return $form->renderForm(
